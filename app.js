6bcb5c19fba4e043fcc9a9e65334264cf452a553
Eli White <ewhite12@uw.edu>
2013-03-20 16:49:17 -0400

    trying to send state

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen(process.env.PORT, process.env.IP);
 
 // routing
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
+});
+
+var state = [1, 0];
+
+io.sockets.on('connection', function (socket) {
+    io.sockets.emit('state', state);
+   
 });
